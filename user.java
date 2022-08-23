6c2fac73304ce3d0790df34c8086246a616be307
antoine carpentier <a.carpentier.wcs@gmail.com>
2022-08-23 21:07:40 +0200

    exo

---
+++ user.java
@@ -0,0 +1,9 @@
+import { Address } from './address';
+import { Photo } from './photo';
+
+public class User {
+   private Date birthdate ;
+   private  Number size;
+   private  Photo[] photos;
+   private  Address address; 
+}
